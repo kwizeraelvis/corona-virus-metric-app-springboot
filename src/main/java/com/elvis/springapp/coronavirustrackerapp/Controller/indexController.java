b12e60aa88f30906e665abaa54cf47c7b0c3ec77
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-17 01:02:43 +0200

    Added TotalCase count attribute to the model

--- src/main/java/com/elvis/springapp/coronavirustrackerapp/Controller/indexController.java
+++ src/main/java/com/elvis/springapp/coronavirustrackerapp/Controller/indexController.java
@@ -1,9 +1,12 @@
 package com.elvis.springapp.coronavirustrackerapp.Controller;
 
+import com.elvis.springapp.coronavirustrackerapp.Domain.VirusStats;
 import com.elvis.springapp.coronavirustrackerapp.Services.CoronaVirusDataService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class indexController {
@@ -16,7 +19,13 @@ public class indexController {
 
     @RequestMapping({"/", "/index"})
     public String index(Model model){
+        List<VirusStats> stats = coronaVirusDataService.getStatsList();
+        int TotalCases = stats
+                .stream()
+                .mapToInt(VirusStats::getLatestCases)
+                .sum();
         model.addAttribute("locationStatistics", coronaVirusDataService.getStatsList());
+        model.addAttribute("TotalCases", TotalCases);
         return "index";
     }
 
