dec225cd9d264890b8368f87541f17d5634c557d
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-17 00:06:08 +0200

    Implement fetch logic

--- src/main/java/com/elvis/springapp/coronavirustrackerapp/Services/CoronaVirusDataService.java
+++ src/main/java/com/elvis/springapp/coronavirustrackerapp/Services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package com.elvis.springapp.coronavirustrackerapp.Services;
 
+import com.elvis.springapp.coronavirustrackerapp.Domain.VirusStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,14 +14,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CoronaVirusDataService {
     private static final String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private List<VirusStats> statsList =  new ArrayList<>();
 
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+        List<VirusStats> stats = new ArrayList<>();
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(DATA_URL))
@@ -29,7 +34,13 @@ public class CoronaVirusDataService {
         StringReader reader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
         for (CSVRecord record : records){
-            String province = record.get("Province/State");
+            VirusStats virusStats = new VirusStats();
+            virusStats.setCountry(record.get("Country/Region"));
+            virusStats.setLatestCases(Integer.parseInt(record.get(record.size() - 1)));
+            virusStats.setState(record.get("Province/State"));
+            System.out.println(virusStats);
+            stats.add(virusStats);
         }
+        this.statsList = stats;
     }
 }
