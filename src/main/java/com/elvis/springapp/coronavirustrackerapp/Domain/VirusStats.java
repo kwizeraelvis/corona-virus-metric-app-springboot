b8ae5d6d3825b22e7f0370d98e8999769baf95bf
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-17 00:05:06 +0200

    Intialise Domain Classes

---
+++ src/main/java/com/elvis/springapp/coronavirustrackerapp/Domain/VirusStats.java
@@ -0,0 +1,31 @@
+package com.elvis.springapp.coronavirustrackerapp.Domain;
+
+public class VirusStats {
+    private String state;
+    private String country;
+    private int latestCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestCases() {
+        return latestCases;
+    }
+
+    public void setLatestCases(int latestCases) {
+        this.latestCases = latestCases;
+    }
+}
