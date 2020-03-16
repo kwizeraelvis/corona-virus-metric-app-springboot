914f332827a93d17bb6c163f88dff747e54efeb5
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-03-16 19:15:33 +0200

    Project BootStrap

---
+++ src/main/java/com/elvis/springapp/coronavirustrackerapp/CoronaVirusTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.elvis.springapp.coronavirustrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVirusTrackerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaVirusTrackerAppApplication.class, args);
+    }
+
+}
